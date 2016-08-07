6bd99ba422629cb30a8152947b528a76b3b2a570
kishore <kishore25kumar@gmail.com>
2016-08-07 09:41:19 +0530

    4th week in between

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -9,3 +9,84 @@
 // program clears the screen, i.e. writes "white" in every pixel.
 
 // Put your code here.
+
+(LOOP)
+	// Loading base address into R3
+	@16384
+	D = A
+	@3
+	M = D
+	// Loading keyboard address
+	@24576
+	D = M
+	@SET0
+	D;JLE
+	@32767
+	D = A
+	@7
+	M = D
+	@SET_SCREEN
+	0;JMP
+	(SET0)
+		@0
+		D = A
+		@7
+		M = D
+		@SET_SCREEN
+		0;JMP
+	(SET_SCREEN)
+	// Initializing 32 and 256 into R0 and R1
+		@32
+		D = A
+		@0
+		M = D
+		@256
+		D = A
+		@1
+		M = D
+		@MUL
+		0;JMP
+		
+		(MUL)
+			@2
+			M = 0
+			(MUL_LOOP)
+				@0
+				D = M
+				@SCREEN_PRINT
+				D;JLE
+				@2
+				D = M
+				@1
+				D = D + M
+				@2
+				M = D
+				@0
+				D = M
+				@1
+				D = D - A
+				@0
+				M = D
+				@MUL_LOOP
+				0;JMP
+		(SCREEN_PRINT)
+			// Decrementing multiplication count by 1
+			@2
+			D = M
+			@1
+			D = D - A
+			@2
+			M = D
+			@LOOP
+			D;JLT
+			@3
+			D = D + M
+			@4
+			M = D
+			@7
+			D = M
+			@4
+			A = M
+			M = D
+			@SCREEN_PRINT
+			0;JMP
--- projects/04/mult/Mult.asm
+++ projects/04/mult/Mult.asm
@@ -7,3 +7,28 @@
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
 // Put your code here.
+
+@2
+M = 0
+(LOOP)
+	@0
+	D = M
+	@END
+	D;JLE
+	@2
+	D = M
+	@1
+	D = D + M
+	@2
+	M = D
+	@0
+	D = M
+	@1
+	D = D - A
+	@0
+	M = D
+	@LOOP
+	0;JMP
+(END)
+	@END
+	0;JMP
